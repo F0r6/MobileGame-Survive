2af03c69a319f85e5717277945d26801b7ffc589
Forg28 <2200768@uad.ac.uk>
2023-08-08 20:52:42 +0100

    Adding Joystick, Tilemap, player base and also starting tilemap procedural generation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody2D), typeof  (BoxCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private float _moveSpeed;
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = new Vector2(_joystick.Horizontal * _moveSpeed, _joystick.Vertical * _moveSpeed);
+    }
+}
